9981c3d4d25b8fc0983b96841c3290d896e18d61
erwing arciniegas castro <90426886+erwingarciniegas@users.noreply.github.com>
2023-10-08 11:41:45 -0500

    Add files via upload

---
+++ generador de contraseñas/Generador2.py
@@ -0,0 +1,12 @@
+import secrets
+import string
+
+def crear_password(longitud):
+    todo_caracteres = string.ascii_letters + string.digits 
+    password = ''
+    for _ in range(longitud):
+        password += secrets.choice(todo_caracteres)
+    return password
+
+nuevo_pass = crear_password(10)
+print(nuevo_pass)
